5feb389cca89a3a1455adf9a1a4b1096d4b8ec34
Lim Jun Hup <limjunhup1997@gmail.com>
2020-02-17 20:40:20 +0800

    Improve responsiveness

--- src/components/TeamMain.js
+++ src/components/TeamMain.js
@@ -1,32 +1,32 @@
-import React from "react"
-import TeamCard from "./TeamCard"
-import { makeStyles } from "@material-ui/core/styles"
-import Paper from "@material-ui/core/Paper"
-import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
-import Divider from "@material-ui/core/Divider"
+import React from 'react';
+import TeamCard from './TeamCard';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(3, 2),
     marginLeft: theme.spacing(6),
-    marginTop: theme.spacing(6),
+    marginTop: theme.spacing(6)
   },
   row: {
-    paddingTop: "30px",
-    justifyContent: "space-evenly",
+    paddingTop: '30px',
+    justifyContent: 'space-evenly'
   },
   title: {
-    fontSize: "18px",
-    fontFamily: "ProximaNova",
-    fontWeight: "bold",
-    paddingBottom: "10px",
-    paddingLeft: "15px",
-  },
-}))
+    fontSize: '18px',
+    fontFamily: 'ProximaNova',
+    fontWeight: 'bold',
+    paddingBottom: '10px',
+    paddingLeft: '15px'
+  }
+}));
 
 const TeamMain = ({ cardsState }) => {
-  const classes = useStyles()
+  const classes = useStyles();
   const cardsList = cardsState.map(card => {
     return (
       <TeamCard
@@ -39,19 +39,19 @@ const TeamMain = ({ cardsState }) => {
         leadsCount={card.leads_count}
         isFavorited={card.is_favorited}
       />
-    )
-  })
+    );
+  });
   return (
     <Paper className={classes.paper}>
       <Grid>
         <Typography className={classes.title}> All Teams </Typography>
       </Grid>
-      <Divider variant="middle" />
-      <Grid container className={classes.row}>
+      <Divider variant='middle' />
+      <Grid container sm={12} className={classes.row}>
         {cardsList}
       </Grid>
     </Paper>
-  )
-}
+  );
+};
 
-export default TeamMain
+export default TeamMain;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,45 @@
-import React from "react"
-import { Grid } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-import TopNavBar from "../components/TopNavBar"
-import SideNavBar from "../components/SideNavBar"
-import TeamMain from "../components/TeamMain"
-import ActivityCard from "../components/ActivityCard"
-import { data } from "../data"
+import TopNavBar from '../components/TopNavBar';
+import SideNavBar from '../components/SideNavBar';
+import TeamMain from '../components/TeamMain';
+import ActivityCard from '../components/ActivityCard';
+import { data } from '../data';
 
 const useStyles = makeStyles({
   mainContainer: {
-    backgroundColor: "#E5E5E5",
-    flexGrow: 1,
+    backgroundColor: '#E5E5E5',
+    flexGrow: 1
   },
   bigContainer: {
-    flex: "1",
+    flex: '1'
   },
   bodyContainer: {
-    flex: "1",
-  },
-})
+    flex: '1'
+  }
+});
 
 const IndexPage = () => {
-  const classes = useStyles()
+  const classes = useStyles();
 
-  const states = ["all", "favourites", "archived"]
-  const [state, setState] = React.useState(0)
+  const states = ['all', 'favourites', 'archived'];
+  const [state, setState] = React.useState(0);
 
   const handleChange = (event, newValue) => {
-    setState(newValue)
-  }
+    setState(newValue);
+  };
 
   const cardsState = data.teams.filter(eachTeam => {
-    if (states[state] === "all") {
-      return true
-    } else if (states[state] === "favourites") {
-      return eachTeam.is_favorited
+    if (states[state] === 'all') {
+      return true;
+    } else if (states[state] === 'favourites') {
+      return eachTeam.is_favorited;
     } else {
-      return eachTeam.is_archived
+      return eachTeam.is_archived;
     }
-  })
+  });
 
   return (
     <div className={classes.mainContainer}>
@@ -47,20 +47,20 @@ const IndexPage = () => {
         <Grid>
           <SideNavBar />
         </Grid>
-        <Grid className={classes.bigContainer}>
+        <Grid sm={12} className={classes.bigContainer}>
           <TopNavBar handleChange={handleChange} />
           <Grid container className={classes.bodyContainer}>
-            <Grid xs={8}>
+            <Grid sm={8}>
               <TeamMain cardsState={cardsState} />
             </Grid>
-            <Grid>
+            <Grid sm={4}>
               <ActivityCard activitiesList={data.activities} />
             </Grid>
           </Grid>
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
